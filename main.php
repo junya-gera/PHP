7ee152b2fe2df5a2e1f9397f9e67a858260b4ecc
junya-gera <aa106921@gmailcom>
2020-05-31 20:11:59 +0900

    #7 heredoc

--- main.php
+++ main.php
@@ -4,4 +4,20 @@ echo 'Hello ' . $name .PHP_EOL;
 echo 'Hi ' . $name .PHP_EOL;
 echo "$name kawaii" .PHP_EOL; #""の中なら変数を呼び出せる
 
+// ここに書いたテキストの改行や字下げは保持したまま、$textに代入される
+// <<<の後は終端記号と呼ばれ、好きな名前をつけて良い
+// $text = <<<'EOT'
+// kudo
+//   wahu-
+// komarichan
+// EOT;
+
+// 中で変数を展開する場合は終端記号に''をつけない(heredoc)
+$text = <<<EOT
+$name kawaii
+
+EOT;
+
+echo $text;
+
 # 中にはHTMLも書けるが、書かない場合は閉じタグは省略すべき
