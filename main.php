b29965fc1a264ce008b24965c9652a553dfb311d
junya-gera <aa106921@gmailcom>
2020-05-31 20:04:34 +0900

    #6 変数

--- main.php
+++ main.php
@@ -1,6 +1,7 @@
 <?php
+$name = 'komarichan'; # 変数は大文字と小文字を区別する
+echo 'Hello ' . $name .PHP_EOL;
+echo 'Hi ' . $name .PHP_EOL;
+echo "$name kawaii" .PHP_EOL; #""の中なら変数を呼び出せる
 
-echo 'Hello komarichan'.PHP_EOL;
-echo 'Hello kurugayasan';
-
- ?> 中にはHTMLも書けるが、書かない場合は閉じタグは省略すべき
+# 中にはHTMLも書けるが、書かない場合は閉じタグは省略すべき
