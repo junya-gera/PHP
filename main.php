8ff6477c159f134c5d8ed8caa709d9d5302139ae
junya-gera <aa106921@gmailcom>
2020-06-01 13:50:20 +0900

    #10 定数 define const

--- main.php
+++ main.php
@@ -4,6 +4,15 @@ echo 'Hello ' . $name .PHP_EOL;
 echo 'Hi ' . $name .PHP_EOL;
 echo "$name kawaii" .PHP_EOL; #""の中なら変数を呼び出せる
 
+echo 2 + '3'; // 数字っぽい文字列を数字として解釈してくれる
+
+// 定数はdefineを使う。大文字で、$がつかない
+define('NAME', 'kudo');
+// constも定数
+const KUDO = 'kudoryafuka';
+echo NAME .PHP_EOL;
+echo KUDO .PHP_EOL;
+
 // ここに書いたテキストの改行や字下げは保持したまま、$textに代入される
 // <<<の後は終端記号と呼ばれ、好きな名前をつけて良い
 // $text = <<<'EOT'
