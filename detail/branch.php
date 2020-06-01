b104a2df5d0596d74af7466e7246fe6162c9b130
junya-gera <aa106921@gmailcom>
2020-06-01 14:18:51 +0900

    #20 for while

--- detail/branch.php
+++ detail/branch.php
@@ -27,8 +27,31 @@ switch($signal){
   case 'green':
     echo 'Go!' .PHP_EOL;
     break;
-    
+
   default:
   echo 'Wrong signal!' .PHP_EOL;
   break;
 }
+
+for ($i = 1; $i <= 5; $i++) {
+  // if ( $i % 2 === 0){
+  //   continue;
+  // }
+  if ( $i === 4){
+  break;
+  }
+  echo "$i Hello" .PHP_EOL;
+}
+
+$hp = -100;
+
+// while ($hp > 0) {
+//   echo "Your HP : $hp" . PHP_EOL;
+//   $hp -= 15;
+// }
+
+do {  // 1度はdoを実行し、その後whileの条件を調べる
+  echo "Your HP : $hp" . PHP_EOL;
+  $hp -= 15;
+
+} while ($hp > 0);
