ce8aed1e855c6da73292fcfe1ead315d2ab2465b
junya-gera <aa106921@gmailcom>
2020-06-26 14:43:09 +0900

    #2 sprintf

--- built-in/built1.php
+++ built-in/built1.php
@@ -3,5 +3,9 @@
 $name = 'Apple';
 $score = 32.246;
 
-$info = "[$name][$score]";
+// sprintf 表示の幅を設定したり、小数点以下の桁数を指定
+// %s:整数 15:桁数 -:左詰
+// %f:浮動小数点数 10.2:10桁、小数点以下2桁 
+$info = sprintf("[%-15s][%010.2f]", $name, $score);
 echo $info . PHP_EOL;
+
