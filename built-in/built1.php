52fab9128262647b9834ecadd034aabc24150f47
junya-gera <aa106921@gmail.com>
2020-06-26 15:02:10 +0900

    #6 substr substr_replace number_format

--- built-in/built1.php
+++ built-in/built1.php
@@ -18,3 +18,18 @@ echo strpos($input, '_'). PHP_EOL; // strpos() 文字列の検索
 
 $input = str_replace('_', '-', $input); // 文字列の置換
 echo $input . PHP_EOL;
+
+$sample = '20200320Item-A  1200';
+// substr_replace() 文字列の置換
+$sample = substr_replace($sample, 'Item-B  ',8, 8);
+
+// substr() 値を切り出す
+$date = substr($sample, 0, 8); // 0~8桁目
+$product = substr($sample, 8, 8);
+$amount = substr($sample, 16); // 16桁目~最後まで
+
+echo $date . PHP_EOL;
+echo $product . PHP_EOL;
+echo number_format($amount) . PHP_EOL; // number_format() 3桁ごとに,を入れる
+
+
