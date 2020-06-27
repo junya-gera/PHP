388f3c86ad0f9a59981287f8880d95d964ce0719
junya-gera <aa106921@gmail.com>
2020-06-27 10:46:19 +0900

    #11 array_slice

--- built-in/built2.php
+++ built-in/built2.php
@@ -8,3 +8,9 @@ $scores[] = 80; // 一つだけ末尾に挿入
 array_shift($scores); // 先頭1つを削除
 array_pop($scores); // 末尾1つを削除
 print_r($scores);
+
+// array_slice() 配列から要素を切り出して別の配列に格納
+$partial = array_slice($scores, 2);
+
+print_r($scores);
+print_r($partial);
