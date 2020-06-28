f7a3e6da7d7e8e3b1c0a381a2a2edf760b790b33
junya-gera <aa106921@gmail.com>
2020-06-28 19:44:10 +0900

    #12 array_splice

--- built-in/built2.php
+++ built-in/built2.php
@@ -9,8 +9,15 @@ array_shift($scores); // 先頭1つを削除
 array_pop($scores); // 末尾1つを削除
 print_r($scores);
 
+
 // array_slice() 配列から要素を切り出して別の配列に格納
 $partial = array_slice($scores, 2);
 
 print_r($scores);
 print_r($partial);
+
+// array_splice() 配列の2番目から3つを削除 
+array_splice($scores, 2, 3);  // 20,30,70
+// 配列の2番目から0個削除し100,101を挿入
+array_splice($scores, 2, 0, [100, 101]);
+print_r($scores);
