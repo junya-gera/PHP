a5a9ceb40f8aca6fd84b5dd2fdb4cee4e55bd05d
junya-gera <aa106921@gmail.com>
2020-06-28 19:52:27 +0900

    #13 sort shuffle array_rand

--- built-in/built2.php
+++ built-in/built2.php
@@ -16,8 +16,21 @@ $partial = array_slice($scores, 2);
 print_r($scores);
 print_r($partial);
 
-// array_splice() 配列の2番目から3つを削除 
+// array_splice() 配列の2番目から3つを削除
+// array_sliceとは違い元の配列を変更する（破壊的）
 array_splice($scores, 2, 3);  // 20,30,70
 // 配列の2番目から0個削除し100,101を挿入
 array_splice($scores, 2, 0, [100, 101]);
 print_r($scores);
+
+
+sort($scores);  // 小さい順
+print_r($scores);
+rsort($scores); // 大きい順
+print_r($scores);
+shuffle($scores); // シャッフル
+print_r($scores);
+// シャッフルして2個をランダムにチョイス
+$picked = array_rand($scores, 2);
+// $pickedだけだとキーが表示される。値を表示するには以下にする
+echo $scores[$picked[0]] . PHP_EOL;
