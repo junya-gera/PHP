ec30d6333d69b97dc8059d508bd89abc01ac3f1c
junya-gera <aa106921@gmail.com>
2020-06-29 12:46:34 +0900

    #15 array_merge array_unique array_intersect

--- built-in/built3.php
+++ built-in/built3.php
@@ -10,3 +10,19 @@ echo array_sum($scores) . PHP_EOL; // 配列の合計
 echo max($scores) . PHP_EOL; // 最大
 echo min($scores) . PHP_EOL; // 最小
 echo array_sum($scores) / count($scores) . PHP_EOL; // 配列の平均
+
+
+$a = [3, 4, 8];
+$b = [4, 8, 12];
+
+$merged = array_merge($a, $b); // array_merge() 配列の結合
+print_r($merged);
+
+$uniques = array_unique($merged); // array_unique() 配列から重複をなくす
+print_r($uniques);
+
+$diff1 = array_diff($a, $b); // aからbにある要素を引く
+print_r($diff1); // 3
+
+$common = array_intersect($a, $b); // aとbの共通のもの
+print_r($common);
