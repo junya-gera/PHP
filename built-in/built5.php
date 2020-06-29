08f55286a8d55214f3883a820ccb6a9aeb108de9
junya-gera <aa106921@gmail.com>
2020-06-29 15:21:19 +0900

    #26 'r' 読み込み fgets

--- built-in/built5.php
+++ built-in/built5.php
@@ -6,13 +6,24 @@
 // あとはファイルポインタを介してファイル操作を行う
 // $fp = fopen('names.txt', 'w');
 // 'a'はappendで更新ではなく追記していく
-$fp = fopen('names.txt', 'a');
+// $fp = fopen('names.txt', 'a');
 
-fwrite($fp, "jiro\n");
-fwrite($fp, "saburo\n");
+// fwrite($fp, "jiro\n");
+// fwrite($fp, "saburo\n");
 
 
 // fwrite() 値を書き込む
-fwrite($fp, "taro\n");
+// fwrite($fp, "taro\n");
+
+// 'r'は読み込み
+$fp = fopen('names.txt', 'r');
+// fread() サイズを指定して一気に読みこむ
+// $contents = fread($fp, filesize('names.txt'));
+
+// fgets() 1行ずつ読み込む。読み込む行がなくなったらfalseを返す
+while (($line = fgets($fp)) !== false){
+  echo $line;
+}
 // fclose() 書き込み終了
 fclose($fp);
+// echo $contents;
