5929e68f69b6ea53f75e86cf86b0bf66a1551557
junya-gera <aa106921@gmail.com>
2020-06-30 17:08:36 +0900

    #2 インスタンスの作成、メソッドの呼び出し

--- object/object1.php
+++ object/object1.php
@@ -13,3 +13,16 @@ class Post
     printf('%s (%s)' . PHP_EOL, $this->text, $this->likes);
   }
 }
+
+$posts = [];
+
+$posts[0] = new Post(); // インスタンス
+$posts[0]->text = 'hello';
+$posts[0]->likes = 0;
+
+$posts[1] = new Post(); // インスタンス
+$posts[1]->text = 'hello again';
+$posts[1]->likes = 0;
+
+$posts[0]->show();
+$posts[1]->show();
