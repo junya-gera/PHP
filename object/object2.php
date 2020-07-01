d6f091218236d2ce7ea3ec3cf43c6c586372e9f3
junya-gera <aa106921@gmail.com>
2020-07-01 11:52:31 +0900

    #10 クラスメソッド static

--- object/object2.php
+++ object/object2.php
@@ -6,22 +6,31 @@ declare(strict_types=1);
 class Post
 {
   private string $text; // 型宣言 string型しか受け付けない
+  private static $count; // クラスプロパティ
 
   public function __construct(string $text)
   {
     $this->text = $text;
+    self::$count++;
   }
 
   public function show()
   {
     printf('%s' . PHP_EOL, $this->text);
   }
+
+  public static function showInfo() // クラスメソッド
+  {
+    printf('Count: %d' . PHP_EOL, self::$count);
+  }
 }
 
 $posts = [];
 
-$posts[0] = new Post('5');
+$posts[0] = new Post('hello');
 $posts[1] = new Post('hello again');
 
 $posts[0]->show();
 $posts[1]->show();
+
+Post::showInfo(); // クラスから直接クラスメソッドを呼び出す
