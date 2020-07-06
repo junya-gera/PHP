785f7d23ecb045406d8f696eebf52ca4e7dee650
junya-gera <aa106921@gmail.com>
2020-07-06 23:55:01 +0900

    #10 ネクストボタンを押すとリロード

--- quiz/quiz.js
+++ quiz/quiz.js
@@ -3,6 +3,10 @@ $(function(){
 
   $('.answer').on('click',function(){
     var $selected = $(this);  // クリックされた答えを$selectedに入れる
+    // 既にボタンが押されていたらもう押せなくする
+    if ($selected.hasClass('correct') || $selected.hasClass('wrong')){
+      return;
+    }
     $selected.addClass('selected');
     var answer = $selected.text(); // 答えのテキストをanswerとして保持
 
@@ -22,6 +26,13 @@ $(function(){
       } else {
         $selected.text(answer + '... WRONG!');
       }
-    })
+      $('#btn').removeClass('disabled'); // ネクストボタンが押せる
+    });
+  });
+
+  $('#btn').on('click', function(){
+    if (!$(this).hasClass('disabled')){ // disabledがなければリロードできる
+      location.reload();
+    }
   })
-})
+});
