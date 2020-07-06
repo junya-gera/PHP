bd06a58fa8b12c6ff76f1bc1445a74bcb6aea7ad
junya-gera <aa106921@gmail.com>
2020-07-06 23:48:28 +0900

    #9 答えを押すと正誤でスタイルが変わる

--- quiz/quiz.js
+++ quiz/quiz.js
@@ -3,17 +3,24 @@ $(function(){
 
   $('.answer').on('click',function(){
     var $selected = $(this);  // クリックされた答えを$selectedに入れる
+    $selected.addClass('selected');
     var answer = $selected.text(); // 答えのテキストをanswerとして保持
 
     $.post('/_answer.php',{ // ajax処理 answer.phpに投げる
 
     // 終わった後の処理はdoneに続けて書く
     }).done(function(res){ // 返ってくるデータをresにする
-      alert(res.correct_answer);
+      $('.answer').each(function(){ // どれかが押された時点で全ての答えに適用
+        if($(this).text() === res.correct_answer){
+          $(this).addClass('correct');
+        } else {
+          $(this).addClass('wrong');
+        }
+      })
       if (answer === res.correct_answer){
-        // correct!
+        $selected.text(answer + '... CORRECT!');
       } else {
-        // wrong!
+        $selected.text(answer + '... WRONG!');
       }
     })
   })
