c0b3b4d9cc2a872b5ba1735585c4c192e8394e3e
junya-gera <aa106921@gmail.com>
2020-07-08 20:08:00 +0900

    paiza C030 白か黒か foreach

--- paiza/rankC.php
+++ paiza/rankC.php
@@ -32,3 +32,47 @@ while($X != 0){
 }
 
 echo $pay;
+
+
+
+// C030 白にするか黒にするか 128以上なら1,127以下なら0
+// 3 2
+// 128 127
+// 127 128
+// 128 127
+
+$input = trim(fgets(STDIN)); // 複数行を配列に
+while ($input){
+$array[] = $input;
+$input = trim(fgets(STDIN));
+}
+$a = explode(" ", $array[0]);
+
+array_shift($array);
+
+$answer_unit = "";
+
+foreach($array as $line){ // 行を1列ずつにする
+    $units = explode(" ", $line); // 数字を1つずつ配列にする
+    
+    foreach($units as $unit){ // 1つずつ128以上か調べる
+        if ($unit >= 128){
+            $str = 1;
+            $answer_unit = $answer_unit . " " . $str; // 1なら$answer_unitに結合
+        } else {
+            $str = 0;
+            $answer_unit = $answer_unit . " " . $str; // 0なら$answer_unitに結合
+        }
+    }
+    $answer[] = $answer_unit; // 1と0の入った列を$answerという配列に入れていく
+    $answer_unit = ""; // 1と0の入った列をリセット
+    
+}
+
+if ($array[0] == 0){
+    echo 0;
+} else {
+    foreach ($answer as $key => $value) {
+    echo trim($value) .PHP_EOL; // 配列のバリューだけを空白を削除して1行ずつ表示
+    }
+}
