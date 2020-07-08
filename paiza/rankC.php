117379bd958783e2d4c6ccdf9d16302b349d7f52
junya-gera <aa106921@gmail.com>
2020-07-08 19:20:14 +0900

    paiza C048 割引率

--- paiza/rankC.php
+++ paiza/rankC.php
@@ -17,3 +17,18 @@ echo chunk_split($str, $int[2]) ."\n"; // $int[2] = 6文字ごとに改行を入
 
 // 最初、何文字で改行を入れるかのところを$array[0]の最後の文字にしていた。
 // こうすると、2桁で改行を入れる場合におかしくなることに気づいた。
+
+
+// C048 タダ飲みコーヒー 割引率をかけていって何円でタダになるか
+$input_line = fgets(STDIN);
+$array = explode(" ", $input_line);
+$X = $array[0];
+$P = $array[1];
+
+$pay = 0;
+while($X != 0){
+    $pay = $pay + $X; // 支払ったお金の総額
+    $X = $X - ceil($X * $P / 100);
+}
+
+echo $pay;
