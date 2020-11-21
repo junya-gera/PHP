a2ba146c7364c53cc97e2ec5c5e454f65fbf1848
Junya WADA <junya-wada@msen.jp>
2020-11-21 17:44:15 +0900

    $fetch で PDOStatement を表示

--- database/main.php
+++ database/main.php
@@ -8,5 +8,19 @@ $pdo = new PDO(
   // user
   'dbuser',
   // password
-  'dbpass'
+  'dbpass',
+  [
+    // $fetch_style カラム名で添え字をつけた配列を返す
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+  ]
 );
+
+// SQL を実行してみる
+// アロー演算子で $pdo の query メソッドを使う
+// query メソッドの結果は PDO ステートメントというオブジェクトで返ってくる
+$stmt = $pdo->query("SELECT 5 + 3");
+
+// fetch() PDOStatement の結果から行を取得する。 $fetch_style を指定してどのように返すか決める
+$result = $stmt->fetch();
+
+var_damp($result);
