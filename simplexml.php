b8800d79637b97e1220a6a7dd3b2e343e604e8bd
Junya WADA <junya-wada@msen.jp>
2020-12-01 22:26:03 +0900

    simplexml_load_file で xml を読み込む

---
+++ simplexml.php
@@ -0,0 +1,36 @@
+<!doctype html>
+<html lang="ja">
+<head>
+<!-- Required meta tags -->
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+<!-- Bootstrap CSS -->
+<link rel="stylesheet" href="css/style.css">
+
+<title>PHP</title>
+</head>
+<body>
+<header>
+<h1 class="font-weight-normal">PHP</h1>    
+</header>
+
+<main>
+<h2>Practice</h2>
+<pre>
+<?php
+// simplexml_load_file xml をオブジェクトで読み込む
+$xmlTree = simplexml_load_file('https://h2o-space.com/feed/');
+var_dump($xmlTree);
+// xml の channel のバリューの配列になっている item を foreach
+foreach ($xmlTree->channel->item as $item):
+?>
+・<a href="<?php print($item->link); ?>"><?php print
+($item->title); ?></a>
+<?php
+endforeach;
+?>
+</pre>
+</main>
+</body>    
+</html>
